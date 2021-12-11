57dd2a5f1bb24ae5872f283a09f8be1622aa2f8f
Yotty0404 <tyuke9216@hotmail.co.jp>
2021-12-11 15:15:05 +0900

    bit全探索もどき　コミット

--- OPC001/Program.cs
+++ OPC001/Program.cs
@@ -19,166 +19,68 @@ namespace OPC001
             var E = int.Parse(input1[2]);
             var EN = int.Parse(input1[3]);
             var L = input2.Select(x => int.Parse(x) - 1).ToList();
-            var L_Origin = new List<int>();
-            foreach (var n in L) L_Origin.Add(n);
-            L.Sort();
-            L.Reverse();
-            var output = "";
+            var L_Output = new List<int>();
+            //Output用のリストは同じサイズ分0で初期化しておく
+            foreach (var n in L) L_Output.Add(0);
 
-            //var output = string.Join(" ", Enumerable.Repeat("1", N));
-
-            var groups = new List<List<int>>();
-            var group = new List<int>();
-            var cnt = 0;
-            var elevator = new List<List<int>>();
-
-            //エレベーターの人数ごとにグループ分け
-            foreach (var n in L)
-            {
-                if (cnt == 0)
-                {
-                    group = new List<int>();
-                }
-                group.Add(n);
-
-                cnt++;
-
-                if (cnt == EN)
-                {
-                    groups.Add(group);
-                    cnt = 0;
-                }
-            }
-
-            output = string.Join(" ", L);
-            //Console.WriteLine(output);
-
-            //最初の10グループをエレベーターに追加
+            var groupNum = E * EN;
+            var timeElevator = new List<int>();
+            //エレベータの時間初期化
             for (int i = 0; i < E; i++)
             {
-                var tempGroup = new List<int>();
-                foreach (var n in groups[i]) tempGroup.Add(n);
-                elevator.Add(tempGroup);
+                timeElevator.Add(0);
             }
 
-
-            var dic = new Dictionary<int, int>();
-
-            for (int i = 0; i < E; i++)
+            var chunkSize = EN;
+            //最後のグループだけ全探索するため、ここでは除外
+            for (int i = 0; i < (N - groupNum) / (groupNum); i++)
             {
-                dic.Add(i, elevator[i].Max());
-            }
-
-            var index = E;
-            while (index < N / EN)
-            {
-                //現状最小のエレベーターの番号を取得
-                var e_min = dic.OrderBy(x => x.Value).FirstOrDefault().Key;
-                elevator[e_min].AddRange(groups[index]);
-
-                //dicを最新の情報で更新
-                //往復分＋人数
-                dic[e_min] += groups[index].Max() * 2 + EN;
-                index++;
-            }
-
-            Func<int> CalcTime = () =>
-            {
-                int rtnTime = 0;
-                int tempTime = 0;
-
-                foreach (var item in elevator)
+                var l = L.GetRange(i * groupNum, groupNum);
+                l.Sort();
+                foreach (var group in l.Chunks(EN))
                 {
-                    tempTime = 0;
-                    item.Sort();
-                    var amari = item.Count % EN;
+                    //現状最小時間のところに加算
+                    var ele_num = timeElevator.FindIndex(x => x == timeElevator.Min());
+                    timeElevator[ele_num] += group.Max() * 2 + EN;
 
-                    if (amari == 0)
+                    foreach (var n in group)
                     {
-                        amari = 5;
+                        var index = L.FindIndex(x => x == n);
+                        //乗るエレベーターを更新
+                        L_Output[index] = ele_num + 1;
+                        //すでにエレベーターに乗った人は0で更新
+                        L[index] = 0;
                     }
-
-                    for (int i = 0; i < item.Count; i++)
-                    {
-                        if (i < item.Count - amari)
-                        {
-                            tempTime += item[i] * 2 + EN;
-                        }
-                        else
-                        {
-                            tempTime += item[i];
-                        }
-                    }
-
-                    rtnTime = System.Math.Max(rtnTime,tempTime);
                 }
+            }
 
-                return rtnTime;
-            };
-
-
-            var tempTimeScore = CalcTime();
-            var nextTimeScore = 0;
-            var tempElevatorIndex = 0;
-            var nextElevatorIndex = 0;
-            var changeIndex = 0;
-
-            var random = new Random(4);
-            var LIMIT_TIME = new TimeSpan(0, 0, 0, 1, 900);
-
-
-            cnt = 0;
-            while (sw.Elapsed < LIMIT_TIME)
+            for (int i = 0; i < Math.Pow(6, 4) * 5 + 1; i++)
             {
-                cnt++;
-                tempElevatorIndex = random.Next(E);
-                nextElevatorIndex = random.Next(E);
-                if (tempElevatorIndex == nextElevatorIndex)
+                var bit0 = (int)(i / Math.Pow(6, 4)) % 6;
+                var bit1 = (int)(i / Math.Pow(6, 3)) % 6;
+                var bit2 = (int)(i / Math.Pow(6, 2)) % 6;
+                var bit3 = (int)(i / Math.Pow(6, 1)) % 6;
+                var bit4 = (int)(i / Math.Pow(6, 0)) % 6;
+
+                if (bit0 + bit1 + bit2 + bit3 + bit4  != 5)
                 {
                     continue;
                 }
-
-                changeIndex = random.Next(elevator[tempElevatorIndex].Count);
-                elevator[nextElevatorIndex].Add(elevator[tempElevatorIndex][changeIndex]);
-                elevator[tempElevatorIndex].RemoveAt(changeIndex);
-
-                nextTimeScore = CalcTime();
-
-                //if (nextScoreTime > tempScoreTime)
-                //{
-                //    //元に戻す
-                //    outputL[index] = tempFloor;
-                //}
-                //else
-                //{
-                //    tempScoreTime = nextScoreTime;
-                //}
+                var test = 1;
             }
-
-            var test = cnt;
-            sw.Stop();
-
-
-            //前後に空白を追加
-            output = " " + string.Join(" ", L_Origin) + " ";
-            var e_num = 1;
-            foreach (var e in elevator)
-            {
-                foreach (var n in e)
-                {
-                    //空白を挟んで置換することで、数字単位で置換
-                    var re = new Regex(" " + n.ToString() + " ");
-                    output = re.Replace(output, " " + e_num.ToString() + " ", 1);
-                }
-
-                e_num++;
-            }
-
-            output = output.Substring(1, output.Length - 2);
-
-            var outputL = output.Split(' ').Select(x => int.Parse(x)).ToList();
-            Console.WriteLine(String.Join(" ", outputL));
         }
     }
 
+    public static class Extensions
+    {
+        // 指定サイズでのチャンクに分割する拡張メソッド
+        public static IEnumerable<IEnumerable<T>> Chunks<T>(this IEnumerable<T> list, int size)
+        {
+            while (list.Any())
+            {
+                yield return list.Take(size);
+                list = list.Skip(size);
+            }
+        }
+    }
 }
