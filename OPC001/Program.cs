df909b52bf07ff4bffa64495ceaf7adb79629328
Yotty0404 <tyuke9216@hotmail.co.jp>
2021-12-12 23:12:36 +0900

    最後のグループをいい感じにするやつ、いったんコミット （コメントアウト）

--- OPC001/Program.cs
+++ OPC001/Program.cs
@@ -36,13 +36,14 @@ namespace OPC001
             var l = new List<int>();
             var chunkSize = EN;
             for (int i = 0; i < (N) / (groupNum); i++)
+            //for (int i = 0; i < (N - groupNum) / (groupNum); i++)
             {
                 l = L.GetRange(i * groupNum, groupNum);
                 l.Sort();
                 foreach (var group in l.Chunks(EN))
                 {
                     //現状最小時間のところに加算
-                    var ele_num = timeElevator.FindIndex(x => x == timeElevator.Min());
+                    var ele_num = timeElevator.FindIndex(x => x == timeElevator.Max());
                     timeElevator[ele_num] += group.Max() * 2 + EN;
 
                     foreach (var n in group)
@@ -57,8 +58,16 @@ namespace OPC001
             }
 
             //l = L.GetRange(N - groupNum, groupNum);
-            //l.Sort();
-            //var groups = l.Chunks(EN).ToList();
+            //var dic = new Dictionary<int, int>();
+
+
+            //for (int i = 0; i < l.Count; i++)
+            //{
+            //    dic.Add(i, l[i]);
+            //}
+            //var sortedDic = dic.OrderBy(x => x.Value).ToList();
+
+            //var groups = sortedDic.Chunks(EN).ToList();
 
             //var tempElevator = new List<List<int>>();
             //var OptimalSolutionTotalTime = int.MaxValue;
@@ -76,19 +85,46 @@ namespace OPC001
             //Func<int> CalcTime = () =>
             //{
             //    var returnTotalTime = 0;
+
             //    for (int i = 0; i < tempElevator.Count; i++)
             //    {
             //        var tempTotalTime = timeElevator[i];
+
+            //        if (tempElevator[i].Count == 0)
+            //        {
+            //            returnTotalTime = Math.Max(returnTotalTime, tempTotalTime);
+            //            continue;
+            //        }
+
+            //        if (tempElevator[i].Count == 1)
+            //        {
+            //            tempTotalTime += groups[tempElevator[i][0]].Max(x => x.Value) + EN;
+            //            returnTotalTime = Math.Max(returnTotalTime, tempTotalTime);
+            //            continue;
+            //        }
+
+            //        //同じエレベーターに複数ある時は、階数順ではなく、並び順で時間を計算する
+            //        var newDic = new Dictionary<int, int>();
+            //        foreach (var n in tempElevator[i])
+            //        {
+            //            foreach (var item in groups[n])
+            //            {
+            //                newDic.Add(item.Key, item.Value);
+            //            }
+            //        }
+
+            //        sortedDic = newDic.OrderBy(x => x.Key).ToList();
+            //        var newGroups = sortedDic.Chunks(EN).ToList();
 
             //        for (int j = 0; j < tempElevator[i].Count; j++)
             //        {
             //            if (j != tempElevator[i].Count - 1)
             //            {
-            //                tempTotalTime += groups[tempElevator[i][j]].Max() * 2 + EN;
+            //                tempTotalTime += newGroups[j].Max(x => x.Value) * 2 + EN;
             //            }
             //            else
             //            {
-            //                tempTotalTime += groups[tempElevator[i][j]].Max() + EN;
+            //                tempTotalTime += newGroups[j].Max(x => x.Value) + EN;
             //            }
             //        }
 
@@ -135,13 +171,40 @@ namespace OPC001
             //{
             //    for (int j = 0; j < OptimalSolutionGroups[elevator_num].Count; j++)
             //    {
-            //        foreach (var n in groups[OptimalSolutionGroups[elevator_num][j]])
+            //        if (OptimalSolutionGroups[elevator_num].Count == 1)
             //        {
-            //            var index = L.FindIndex(x => x == n);
-            //            //乗るエレベーターを更新
-            //            L_Output[index] = elevator_num + 1;
-            //            //すでにエレベーターに乗った人は0で更新
-            //            L[index] = 0;
+            //            foreach (var n in groups[OptimalSolutionGroups[elevator_num][j]])
+            //            {
+            //                var index = L.FindIndex(x => x == n.Value);
+            //                //乗るエレベーターを更新
+            //                L_Output[index] = elevator_num + 1;
+            //                //すでにエレベーターに乗った人は0で更新
+            //                L[index] = 0;
+            //            }
+            //        }
+            //        else
+            //        {
+            //            //同じエレベーターに複数ある時は、階数順ではなく、並び順で時間を計算する
+            //            var newDic = new Dictionary<int, int>();
+            //            foreach (var n in OptimalSolutionGroups[elevator_num])
+            //            {
+            //                foreach (var item in groups[n])
+            //                {
+            //                    newDic.Add(item.Key, item.Value);
+            //                }
+            //            }
+
+            //            sortedDic = newDic.OrderBy(x => x.Key).ToList();
+            //            var newGroups = sortedDic.Chunks(EN).ToList();
+
+            //            foreach (var n in newGroups[j])
+            //            {
+            //                var index = L.FindIndex(x => x == n.Value);
+            //                //乗るエレベーターを更新
+            //                L_Output[index] = elevator_num + 1;
+            //                //すでにエレベーターに乗った人は0で更新
+            //                L[index] = 0;
+            //            }
             //        }
             //    }
             //}
